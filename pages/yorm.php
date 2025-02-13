c8e4f0ca49c05262842dea79d6433032cec05804
Thomas Skerbis <thomas.skerbis@mac.com>
2025-02-13 21:23:44 +0100

    Update yorm.php

--- pages/yorm.php
+++ pages/yorm.php
@@ -40,7 +40,7 @@ if ($selectedTable && !empty($columns)) {
     $yformTable = rex_sql::factory()->getArray('SELECT * FROM rex_yform_table WHERE table_name = :table', ['table' => $selectedTable])[0];
     $fields = rex_sql::factory()->getArray('SELECT * FROM rex_yform_field WHERE table_name = :table ORDER BY prio', ['table' => $selectedTable]);
     
-    // Generate YORM code
+    // Generate model code
     $className = 'Rex' . str_replace(' ', '', ucwords(str_replace(['rex_', '_'], ['',' '], $selectedTable)));
     
     $modelCode = [];
@@ -55,57 +55,47 @@ if ($selectedTable && !empty($columns)) {
     foreach ($fields as $field) {
         if ($field['type_id'] == 'value') {
             $name = $field['name'];
-            $label = $field['label'];
-            $type = $field['type_name'];
+            $label = $field['label'] ?: $name;
             
-            $phpType = 'mixed';
-            switch ($type) {
-                case 'text':
-                case 'textarea':
-                case 'email':
-                case 'upload':
-                    $phpType = 'string';
-                    break;
-                case 'checkbox':
-                    $phpType = 'bool';
-                    break;
-                case 'int':
-                case 'integer':
-                    $phpType = 'int';
-                    break;
-                case 'float':
-                case 'decimal':
-                    $phpType = 'float';
-                    break;
-                case 'datetime':
-                    $phpType = '?\\DateTime';
-                    break;
-                case 'be_manager_relation':
-                    $phpType = '?self';
-                    break;
-            }
-            
-            // Add getter method
             $methodName = 'get' . str_replace(' ', '', ucwords(str_replace('_', ' ', $name)));
+            
+            $typeMap = [
+                'text' => 'string',
+                'textarea' => 'string',
+                'select' => 'string',
+                'checkbox' => 'bool',
+                'radio' => 'bool',
+                'email' => 'string',
+                'integer' => 'int',
+                'float' => 'float',
+                'decimal' => 'float',
+                'date' => '?\\DateTime',
+                'datetime' => '?\\DateTime',
+                'time' => '?\\DateTime',
+                'be_link' => 'int',
+                'be_media' => 'string',
+                'be_medialist' => 'string',
+                'be_manager_relation' => 'rex_yform_manager_collection|null',
+                'choice' => 'string',
+            ];
+            
+            $type = $typeMap[$field['type_name']] ?? 'mixed';
+            
             $modelCode[] = '    /**';
-            $modelCode[] = '     * ' . ($label ?: $name);
-            if ($type == 'be_manager_relation') {
-                $modelCode[] = '     * @return rex_yform_manager_collection|' . $phpType;
-            } else {
-                $modelCode[] = '     * @return ' . $phpType;
-            }
+            $modelCode[] = '     * ' . $label;
+            $modelCode[] = '     * @return ' . $type;
             $modelCode[] = '     */';
             
-            if ($type == 'datetime') {
+            if (in_array($field['type_name'], ['date', 'datetime', 'time'])) {
                 $modelCode[] = '    public function ' . $methodName . '()';
                 $modelCode[] = '    {';
                 $modelCode[] = '        $value = $this->getValue(\'' . $name . '\');';
                 $modelCode[] = '        return $value ? new \\DateTime($value) : null;';
                 $modelCode[] = '    }';
-            } elseif ($type == 'be_manager_relation') {
+            } elseif ($field['type_name'] === 'be_manager_relation') {
                 $modelCode[] = '    public function ' . $methodName . '()';
                 $modelCode[] = '    {';
-                $modelCode[] = '        return $this->getRelatedDataset(\'' . $name . '\');';
+                $modelCode[] = '        return $this->getRelatedCollection(\'' . $name . '\');';
                 $modelCode[] = '    }';
             } else {
                 $modelCode[] = '    public function ' . $methodName . '()';
@@ -117,163 +107,128 @@ if ($selectedTable && !empty($columns)) {
         }
     }
     
-    // Generate static factory methods
+    // Generate static finder methods
     $modelCode[] = '    /**';
-    $modelCode[] = '     * @return static[]';
+    $modelCode[] = '     * @return rex_yform_manager_collection|' . $className . '[]';
     $modelCode[] = '     */';
     $modelCode[] = '    public static function getAll()';
     $modelCode[] = '    {';
-    $modelCode[] = '        return static::query()->find();';
+    $modelCode[] = '        return self::query()->find();';
     $modelCode[] = '    }';
     $modelCode[] = '';
     $modelCode[] = '    /**';
-    $modelCode[] = '     * @return static|null';
+    $modelCode[] = '     * @return ' . $className . '|null';
     $modelCode[] = '     */';
     $modelCode[] = '    public static function getById($id)';
     $modelCode[] = '    {';
-    $modelCode[] = '        return static::get($id);';
+    $modelCode[] = '        return self::get($id);';
     $modelCode[] = '    }';
     $modelCode[] = '';
-    
-    // Example usage
-    $modelCode[] = '    // Example usage:';
-    $modelCode[] = '    public static function example()';
-    $modelCode[] = '    {';
-    $modelCode[] = '        // Get all items';
-    $modelCode[] = '        $items = self::getAll();';
-    $modelCode[] = '';
-    $modelCode[] = '        // Get single item by ID';
-    $modelCode[] = '        $item = self::getById(1);';
-    $modelCode[] = '';
-    $modelCode[] = '        // Create new item';
-    $modelCode[] = '        $item = self::create();';
-    
-    // Example setValue for each field
-    foreach ($fields as $field) {
-        if ($field['type_id'] == 'value') {
-            $name = $field['name'];
-            $type = $field['type_name'];
-            
-            switch ($type) {
-                case 'text':
-                case 'textarea':
-                    $modelCode[] = '        $item->setValue(\'' . $name . '\', \'example\');';
-                    break;
-                case 'checkbox':
-                    $modelCode[] = '        $item->setValue(\'' . $name . '\', true);';
-                    break;
-                case 'int':
-                case 'integer':
-                    $modelCode[] = '        $item->setValue(\'' . $name . '\', 42);';
-                    break;
-                case 'datetime':
-                    $modelCode[] = '        $item->setDateTimeValue(\'' . $name . '\', time());';
-                    break;
-            }
-        }
-    }
-    
-    $modelCode[] = '        $item->save();';
-    $modelCode[] = '';
-    $modelCode[] = '        // Custom query';
-    $modelCode[] = '        $items = self::query()';
-    $modelCode[] = '            ->where(\'status\', 1)';
-    $modelCode[] = '            ->orderBy(\'name\')';
-    $modelCode[] = '            ->find();';
-    $modelCode[] = '    }';
     $modelCode[] = '}';
     
-    // Show generated code
+    // Show generated model code
     $fragment = new rex_fragment();
     $fragment->setVar('title', 'YORM Model für ' . $selectedTable);
     $fragment->setVar('body', '
-        <p>Speichern Sie diesen Code als <code>' . $className . '.php</code></p>
+        <div class="alert alert-info">
+            Speichern Sie diesen Code als <code>' . $className . '.php</code>
+        </div>
         <pre class="pre-scrollable"><code class="php">'.rex_escape(implode("\n", $modelCode)).'</code></pre>
-        <button class="btn btn-default" onclick="copyToClipboard()">
-            <i class="rex-icon fa-copy"></i> In Zwischenablage kopieren
+        <button class="btn btn-default" onclick="copyModelCode()">
+            <i class="rex-icon fa-copy"></i> Model-Code kopieren
         </button>
         <script>
-        function copyToClipboard() {
-            const code = document.querySelector("pre code").textContent;
+        function copyModelCode() {
+            const code = document.querySelector("#model-code code").textContent;
             navigator.clipboard.writeText(code).then(() => {
-                alert("Code wurde in die Zwischenablage kopiert!");
+                alert("Model-Code wurde in die Zwischenablage kopiert!");
             });
         }
         </script>
     ', false);
     $content .= $fragment->parse('core/page/section.php');
     
-    // Generate Example Code
-    $usageCode = [];
-    $usageCode[] = '<?php';
-    $usageCode[] = '';
-    $usageCode[] = '// Beispiel für die Verwendung des Models';
-    $usageCode[] = '';
-    $usageCode[] = '// Alle Datensätze abrufen';
-    $usageCode[] = '$items = ' . $className . '::getAll();';
-    $usageCode[] = '';
-    $usageCode[] = '// Einzelnen Datensatz per ID laden';
-    $usageCode[] = '$item = ' . $className . '::getById(1);';
-    $usageCode[] = 'if ($item) {';
+    // Generate form usage code
+    $formCode = [];
+    $formCode[] = '<?php';
+    $formCode[] = '// Neuer Datensatz';
+    $formCode[] = '$dataset = rex_yform_manager_dataset::create("'.$selectedTable.'");';
+    $formCode[] = '';
+    $formCode[] = '// YForm Objekt erstellen';
+    $formCode[] = '$yform = $dataset->getForm();';
+    $formCode[] = '';
+    $formCode[] = '// Formular konfigurieren';
+    $formCode[] = '$yform->setObjectparams(\'form_action\', rex_getUrl(REX_ARTICLE_ID));';
+    $formCode[] = '$yform->setObjectparams(\'form_showformafterupdate\', false);';
+    $formCode[] = '$yform->setObjectparams(\'main_id\', -1);';
+    $formCode[] = '$yform->setObjectparams(\'getdata\', false);';
+    $formCode[] = '';
+    $formCode[] = '// Formular ausgeben';
+    $formCode[] = 'echo $dataset->executeForm($yform);';
     
-    foreach ($fields as $field) {
-        if ($field['type_id'] == 'value') {
-            $name = $field['name'];
-            $methodName = 'get' . str_replace(' ', '', ucwords(str_replace('_', ' ', $name)));
-            $usageCode[] = '    $' . $name . ' = $item->' . $methodName . '();';
-        }
-    }
+    // Generate edit code
+    $editCode = [];
+    $editCode[] = '<?php';
+    $editCode[] = '// Datensatz laden (ID z.B. über rex_get)';
+    $editCode[] = '$dataset = rex_yform_manager_dataset::get(rex_get(\'id\', \'int\'), "'.$selectedTable.'");';
+    $editCode[] = '';
+    $editCode[] = 'if ($dataset) {';
+    $editCode[] = '    // YForm Objekt erstellen';
+    $editCode[] = '    $yform = $dataset->getForm();';
+    $editCode[] = '    ';
+    $editCode[] = '    // Formular konfigurieren';
+    $editCode[] = '    $yform->setObjectparams(\'form_action\', rex_getUrl(REX_ARTICLE_ID));';
+    $editCode[] = '    $yform->setObjectparams(\'form_showformafterupdate\', false);';
+    $editCode[] = '';
+    $editCode[] = '    // Formular ausgeben';
+    $editCode[] = '    echo $dataset->executeForm($yform);';
+    $editCode[] = '}';
     
-    $usageCode[] = '}';
-    $usageCode[] = '';
-    $usageCode[] = '// Neuen Datensatz erstellen';
-    $usageCode[] = '$item = ' . $className . '::create();';
+    // Generate list code
+    $listCode = [];
+    $listCode[] = '<?php';
+    $listCode[] = '// Datensätze laden';
+    $listCode[] = '$items = rex_yform_manager_dataset::query("'.$selectedTable.'")->find();';
+    $listCode[] = '';
+    $listCode[] = 'if ($items->count() > 0) {';
+    $listCode[] = '    foreach ($items as $item) {';
+    $listCode[] = '        // Datensatz bearbeiten Link';
+    $listCode[] = '        $editUrl = rex_getUrl(REX_ARTICLE_ID, REX_CLANG_ID, [\'id\' => $item->getId()]);';
+    $listCode[] = '        echo \'<a href="\'.$editUrl.\'">Bearbeiten</a>\';';
+    $listCode[] = '    }';
+    $listCode[] = '}';
     
-    foreach ($fields as $field) {
-        if ($field['type_id'] == 'value') {
-            $name = $field['name'];
-            $type = $field['type_name'];
-            
-            switch ($type) {
-                case 'text':
-                case 'textarea':
-                    $usageCode[] = '$item->setValue(\'' . $name . '\', \'Wert\');';
-                    break;
-                case 'checkbox':
-                    $usageCode[] = '$item->setValue(\'' . $name . '\', true);';
-                    break;
-                case 'int':
-                case 'integer':
-                    $usageCode[] = '$item->setValue(\'' . $name . '\', 42);';
-                    break;
-                case 'datetime':
-                    $usageCode[] = '$item->setDateTimeValue(\'' . $name . '\', time());';
-                    break;
-            }
-        }
-    }
-    
-    $usageCode[] = '$item->save();';
-    $usageCode[] = '';
-    $usageCode[] = '// Datensätze mit Query API suchen';
-    $usageCode[] = '$items = ' . $className . '::query()';
-    $usageCode[] = '    ->where(\'status\', 1)';
-    $usageCode[] = '    ->orderBy(\'name\')';
-    $usageCode[] = '    ->find();';
-    
-    // Show example code
+    // Show form usage code
     $fragment = new rex_fragment();
-    $fragment->setVar('title', 'Beispiel Code');
+    $fragment->setVar('title', 'YORM Formular Code');
     $fragment->setVar('body', '
-        <pre class="pre-scrollable"><code class="php">'.rex_escape(implode("\n", $usageCode)).'</code></pre>
-        <button class="btn btn-default" onclick="copyExampleToClipboard()">
-            <i class="rex-icon fa-copy"></i> In Zwischenablage kopieren
+        <div class="nav-tabs">
+            <ul class="nav nav-tabs">
+                <li class="active"><a data-toggle="tab" href="#add">Datensatz anlegen</a></li>
+                <li><a data-toggle="tab" href="#edit">Datensatz bearbeiten</a></li>
+                <li><a data-toggle="tab" href="#list">Datensätze auflisten</a></li>
+            </ul>
+            <div class="tab-content">
+                <div id="add" class="tab-pane active">
+                    <pre class="pre-scrollable"><code class="php">'.rex_escape(implode("\n", $formCode)).'</code></pre>
+                </div>
+                <div id="edit" class="tab-pane">
+                    <pre class="pre-scrollable"><code class="php">'.rex_escape(implode("\n", $editCode)).'</code></pre>
+                </div>
+                <div id="list" class="tab-pane">
+                    <pre class="pre-scrollable"><code class="php">'.rex_escape(implode("\n", $listCode)).'</code></pre>
+                </div>
+            </div>
+        </div>
+        <button class="btn btn-default" onclick="copyFormCode()">
+            <i class="rex-icon fa-copy"></i> Code in Zwischenablage kopieren
         </button>
         <script>
-        function copyExampleToClipboard() {
-            const code = document.querySelectorAll("pre code")[1].textContent;
+        function copyFormCode() {
+            const activeTab = document.querySelector(".tab-pane.active");
+            const code = activeTab.querySelector("code").textContent;
             navigator.clipboard.writeText(code).then(() => {
-                alert("Beispiel-Code wurde in die Zwischenablage kopiert!");
+                alert("Code wurde in die Zwischenablage kopiert!");
             });
         }
         </script>
