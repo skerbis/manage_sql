108f09a3cf6c3bb2fb394f99ad979931256c48d3
Thomas Skerbis <thomas.skerbis@mac.com>
2025-02-14 17:06:53 +0100

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -1,4 +1,4 @@
 <?php
 $package = rex_addon::get('table_manager');
-echo rex_view::title($package->i18n('table_manager'));
+echo rex_view::title($package->i18n('manage_sql'));
 rex_be_controller::includeCurrentPageSubPath();
