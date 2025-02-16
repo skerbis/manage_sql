0076ddca73204ed395546077ab79b3af29aac74c
Thomas Skerbis <thomas.skerbis@mac.com>
2025-02-16 17:36:28 +0100

    Create column_selection.php

---
+++ fragments/column_selection.php
@@ -0,0 +1,28 @@
+<?php
+/** @var string $table */
+/** @var list<string> $columns */
+/** @var list<string> $selectedColumns */
+?>
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <div class="panel-title"><?= $table ?></div>
+    </div>
+    <div class="panel-body">
+        <div class="rex-select-style">
+            <?php foreach ($columns as $column): ?>
+                <div class="checkbox">
+                    <label>
+                        <a href="<?= rex_url::currentBackendPage([
+                            'func' => 'toggle_column',
+                            'table' => $table,
+                            'column' => $column
+                        ]) ?>" class="<?= in_array($column, $selectedColumns) ? 'text-primary' : 'text-muted' ?>">
+                            <i class="rex-icon <?= in_array($column, $selectedColumns) ? 'fa-check-square-o' : 'fa-square-o' ?>"></i>
+                            <?= $column ?>
+                        </a>
+                    </label>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</div>
